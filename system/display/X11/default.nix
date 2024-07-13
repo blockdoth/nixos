38e6e7bf85e1f4902b47ae2e2ec5cc4159132fd2
PepijnVanEgmond <pepijn.pve@gmail.com>
2024-07-13 10:51:50 +0200

    trying to make it compile

--- system/display/X11/default.nix
+++ system/display/X11/default.nix
@@ -2,8 +2,10 @@
 {
   services.xserver = {
     enable = true;
-    layout = "us";
-    xkbVariant = "";
+    xkb = {
+      variant = "";
+      layout = "us";
+    };
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };  
