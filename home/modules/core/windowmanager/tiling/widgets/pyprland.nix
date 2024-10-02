195c9e63c48e9cc994e805fd57619cfdd09ceadb
blockdoth <pepijn.pve@gmail.com>
2024-10-02 11:56:18 +0200

    scratchpad issues

--- home/modules/core/windowmanager/tiling/widgets/pyprland.nix
+++ home/modules/core/windowmanager/tiling/widgets/pyprland.nix
@@ -13,7 +13,6 @@
     home.file."/home/blockdoth/.config/hypr/pyprland.toml".text = ''
       [pyprland]
       plugins = [
-      #  "expose", # broken
        "magnify",
        "scratchpads",
       ]
@@ -24,15 +23,12 @@
       command = "alacritty --class scratchpad.alacritty"
       class = "scratchpad.alacritty"
       size = "90% 90%"
-      # lazy = "true"
       
-
       [scratchpads.spotify]
       animation = "fromBottom"
       hide_delay = 0.5
       command = "spotify --class scratchpad.spotify"
       class = "scratchpad.spotify"
-      # lazy = "true"
     '';
   };
 }
