09ba024ce36c1e793c480e99a56ad41b0eeaec58
blockdoth <pepijn.pve@gmail.com>
2024-11-12 13:21:07 +0100

    [laptop@blockdoth] (2024-11-12 13:21) Home Generation 317

--- home/modules/core/windowmanager/tiling/hyprland/windowrules.nix
+++ home/modules/core/windowmanager/tiling/hyprland/windowrules.nix
@@ -36,6 +36,7 @@
         "float,                     $scratchpad"
         "workspace special silent,  $scratchpad"
         "center,                    $scratchpad"
+        "opacity 0.8,               $scratchpad"
 
         # make discord not steal focus
         "noinitialfocus, class:(vesktop)"
