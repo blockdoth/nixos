bfce2d4ab1378c05add96ac2df520f68a28d1769
blockdoth <pepijn.pve@gmail.com>
2024-11-21 12:00:21 +0100

    [laptop@blockdoth] (2024-11-21 12:00) Home Generation 332

--- home/modules/core/utils/git.nix
+++ home/modules/core/utils/git.nix
@@ -20,6 +20,7 @@
         enable = true;
         userName = "blockdoth";
         userEmail = "pepijn.pve@gmail.com";
+        lfs.enable = true;
         extraConfig = {
           init.defaultBranch = "main";
           push.autoSetupRemote = "true";
