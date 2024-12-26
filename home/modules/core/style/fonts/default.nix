f336ccde7eac96fb5eda62b213c46a00574e6b40
blockdoth <pepijn.pve@gmail.com>
2024-12-26 15:06:30 +0100

    [desktop@blockdoth] (2024-12-26 13:57) Home Generation 717

--- home/modules/core/style/fonts/default.nix
+++ home/modules/core/style/fonts/default.nix
@@ -9,22 +9,32 @@
     modules.core.style.fonts.enable = lib.mkEnableOption "Enables custom fonts";
   };
 
-  config = lib.mkIf config.modules.core.style.fonts.enable {
-    fonts.fontconfig.enable = true;
-    home.packages = with pkgs; [
-      font-manager
-      jetbrains-mono
-      font-awesome
-      powerline-fonts
-      powerline-symbols
-      dejavu_fonts
-      (nerdfonts.override {
-        fonts = [
-          "FiraCode"
-          "DroidSansMono"
-          "JetBrainsMono"
-        ];
-      })
-    ];
-  };
+  config =
+    let
+      local-fonts = pkgs.stdenv.mkDerivation rec {
+        pname = local-fonts;
+        version = "1.0";
+        src = ./local;
+        installPhase = ''find ${src} -type f -name '*.ttf' -exec install -Dm644 {} $out/share/fonts/truetype '';
+      };
+
+    in
+    lib.mkIf config.modules.core.style.fonts.enable {
+      fonts.fontconfig.enable = true;
+      home.packages = with pkgs; [
+        font-manager
+        jetbrains-mono
+        font-awesome
+        powerline-fonts
+        powerline-symbols
+        dejavu_fonts
+        (nerdfonts.override {
+          fonts = [
+            "FiraCode"
+            "DroidSansMono"
+            "JetBrainsMono"
+          ];
+        })
+      ];
+    };
 }
