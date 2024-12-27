340c8c120bde77b90e7bcd69006daeccce9b4804
blockdoth <pepijn.pve@gmail.com>
2024-12-27 16:48:34 +0100

    [desktop@blockdoth] (2024-12-27 16:48) Home Generation 754

--- home/modules/core/terminal/ghostty.nix
+++ home/modules/core/terminal/ghostty.nix
@@ -10,16 +10,24 @@
     modules.core.terminal.ghostty.enable = lib.mkEnableOption "Enables ghostty";
   };
 
-  config = lib.mkIf config.modules.core.terminal.ghostty.enable {
-    home.packages = with pkgs; [ inputs.ghostty.packages.x86_64-linux.default ];
+  config =
+    let
+      stylix = config.lib.stylix;
+    in
+    lib.mkIf config.modules.core.terminal.ghostty.enable {
+      home.packages = with pkgs; [ inputs.ghostty.packages.x86_64-linux.default ];
 
-    xdg.configFile."ghostty/config".text = ''
-      window-decoration = false
-      term = xterm-256color
-      background-opacity = 0.7
-      window-padding-x = 10
-      window-padding-y = 10
-    '';
-  };
+      xdg.configFile."ghostty/config".text = ''
+        window-decoration = false
+        term = xterm-256color
+
+        window-padding-x = 10
+        window-padding-y = 10
+        background = #${stylix.colors.base00}
+        foreground = #${stylix.colors.base05}
+        selection-foreground = #${stylix.colors.base05} 
+        selection-background = #${stylix.colors.base02}     
+      '';
+    };
 
 }
