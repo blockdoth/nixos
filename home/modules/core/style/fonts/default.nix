870df18bc393a3908f98462aab9645ffe66cce82
blockdoth <pepijn.pve@gmail.com>
2024-12-27 14:06:14 +0100

    [desktop@blockdoth] (2024-12-27 14:06) Home Generation 746

--- home/modules/core/style/fonts/default.nix
+++ home/modules/core/style/fonts/default.nix
@@ -12,21 +12,22 @@
   config =
     let
       mkFontDerivation =
-        fontPath: name:
+        fontPath: name: type:
         pkgs.stdenvNoCC.mkDerivation {
           pname = name;
           version = "1.0";
           dontUnpack = true;
           installPhase = ''
-            install -Dm755 ${fontPath} $out/share/fonts/truetype/'${name}'
+            install -Dm755 ${fontPath} $out/share/fonts/${type}/'${name}'
           '';
         };
     in
     lib.mkIf config.modules.core.style.fonts.enable {
       fonts.fontconfig.enable = true;
       home.packages = with pkgs; [
-        (mkFontDerivation ./local/xkcd-script.ttf "xkcd Script")
-        (mkFontDerivation ./local/Comic-Sans-MS.ttf "Comic Sans MS")
+        (mkFontDerivation ./local/xkcd-script.ttf "xkcd Script" "truetype")
+        (mkFontDerivation ./local/Comic-Sans-MS.ttf "Comic Sans MS" "truetype")
+        (mkFontDerivation ./local/xkcd.otf "xkcd" "opentype")
         font-manager
         jetbrains-mono
         font-awesome
