16db91cbe4ee7392bdec7c8767c4743c9937d65d
blockdoth <pepijn.pve@gmail.com>
2025-01-11 23:32:16 +0100

    [desktop@blockdoth] (2025-01-11 23:32) Home Generation 854

--- home/modules/core/utils/secrets.nix
+++ home/modules/core/utils/secrets.nix
@@ -18,7 +18,11 @@
     sops = {
       defaultSopsFile = ../../../../secrets/secrets.yaml;
       defaultSopsFormat = "yaml";
-      age.keyFile = "/home/blockdoth/.config/sops/age/keys.txt"; # TODO make user agnostic
+      age = {
+        keyFile = "/var/lib/sops-nix/key.txt";
+        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        generateKey = true;
+      };
       secrets = {
         atuin-key = { };
       };
