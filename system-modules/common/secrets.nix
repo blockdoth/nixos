a0a44fafef1d35cbde63b43914c20eee0d4a5299
blockdoth <pepijn.pve@gmail.com>
2025-01-12 11:59:50 +0100

    [desktop@blockdoth] (2025-01-12 11:59) System Generation 137

--- system-modules/common/secrets.nix
+++ system-modules/common/secrets.nix
@@ -26,8 +26,8 @@
       defaultSopsFormat = "yaml";
       age = {
         keyFile = "/home/blockdoth/.config/sops/age/keys.txt"; # TODO make user agnostic
-        # sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        # generateKey = true;
+        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        generateKey = true;
       };
       secrets = {
         "users/penger/password" = { };
