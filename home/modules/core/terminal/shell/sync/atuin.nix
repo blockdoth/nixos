a8b4c5f2ab25291c41e436ede2d9f985291db2ed
blockdoth <pepijn.pve@gmail.com>
2025-01-12 19:56:47 +0100

    [desktop@blockdoth] (2025-01-12 19:56) Home Generation 863

--- home/modules/core/terminal/shell/sync/atuin.nix
+++ home/modules/core/terminal/shell/sync/atuin.nix
@@ -13,7 +13,7 @@
   config = lib.mkIf config.modules.core.terminal.shell.sync.atuin.enable {
     programs.atuin = {
       enable = true;
-      enableFishIntegration = false;
+      enableFishIntegration = true;
       settings = {
         auto_sync = true;
         # sync_address = "atuin.insinuatis.ddns.net";
@@ -21,6 +21,10 @@
         sync_frequency = "5m";
         key_path = config.sops.secrets.atuin-key.path;
       };
+      flags = [
+        "--disable-up-arrow"
+        "--disable-ctrl-r"
+      ];
     };
   };
 }
