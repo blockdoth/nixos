e879567c928e6b630e9af67c90f9db2689b31fa5
blockdoth <pepijn.pve@gmail.com>
2025-01-13 09:16:23 +0100

    [desktop@blockdoth] (2025-01-13 09:16) Home Generation 865

--- home/modules/core/utils/secrets.nix
+++ home/modules/core/utils/secrets.nix
@@ -19,7 +19,7 @@
       defaultSopsFile = ../../../../secrets/secrets.yaml;
       defaultSopsFormat = "yaml";
       age = {
-        sshKeyPaths = [ "${config.home.homeDirectory}/.ssh" ];
+        sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519.pub" ];
         generateKey = true;
       };
       secrets = {
