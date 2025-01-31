7d6cb79ebdccb4ecbfa9f46773bf2b61e97a5102
blockdoth <pepijn.pve@gmail.com>
2025-02-01 00:17:08 +0100

    [desktop@blockdoth] (2025-02-01 00:17) System Generation 179

--- system-modules/services/grafana.nix
+++ system-modules/services/grafana.nix
@@ -10,32 +10,44 @@
   };
   # cool snippet for the full stack
   # https://gist.github.com/rickhull/895b0cb38fdd537c1078a858cf15d63e
-  config = lib.mkIf config.system-modules.services.grafana.enable {
-    services.grafana = {
-      enable = true;
-      settings = {
-        analytics.reporting_enabled = false;
-        server = {
-          http_addr = "127.0.0.1";
-          http_port = 3000;
+  config =
+    let
+      domain = "insinuatis.com";
+    in
+
+    lib.mkIf config.system-modules.services.grafana.enable {
+      services.grafana = {
+        enable = true;
+        settings = {
+          analytics.reporting_enabled = false;
+          server = {
+            http_addr = "127.0.0.1";
+            http_port = 3000;
+          };
+        };
+        # security = {
+        #   admin_user = "";
+        #   admin_password = "";
+        # };
+        provision = {
+          enable = true;
+          datasources.settings.datasources = [
+            {
+              name = "Prometheus";
+              type = "prometheus";
+              access = "proxy";
+              editable = false;
+              url = "http://127.0.0.1:${builtins.toString config.services.prometheus.port}";
+            }
+          ];
         };
       };
-      # security = {
-      #   admin_user = "";
-      #   admin_password = "";
-      # };
-      provision = {
+
+      services.caddy = {
         enable = true;
-        datasources.settings.datasources = [
-          {
-            name = "Prometheus";
-            type = "prometheus";
-            access = "proxy";
-            editable = false;
-            url = "http://127.0.0.1:${builtins.toString config.services.prometheus.port}";
-          }
-        ];
+        virtualHosts."grafana.${domain}".extraConfig = ''
+          reverse_proxy http://127.0.0.1:${builtins.toString config.services.grafana.settings.server.http_port}        
+        '';
       };
     };
-  };
 }
--- system-modules/services/prometheus.nix
+++ system-modules/services/prometheus.nix
@@ -9,69 +9,39 @@
     system-modules.services.prometheus.enable = lib.mkEnableOption "Enables prometheus";
   };
 
-  config = lib.mkIf config.system-modules.services.prometheus.enable {
-    services.prometheus = {
-      enable = true;
-      port = 3020;
-      exporters = {
-        node = {
-          port = 3021;
-          enabledCollectors = [ "systemd" ];
-          enable = true;
+  config =
+    let
+      domain = "insinuatis.com";
+    in
+    lib.mkIf config.system-modules.services.prometheus.enable {
+      services.prometheus = {
+        enable = true;
+        port = 3020;
+        exporters = {
+          node = {
+            port = 3021;
+            enabledCollectors = [ "systemd" ];
+            enable = true;
+          };
         };
+        scrapeConfigs = [
+          {
+            job_name = "nodes";
+            static_configs = [
+              {
+                targets = [
+                  "127.0.0.1:${toString config.services.prometheus.exporters.node.port}"
+                ];
+              }
+            ];
+          }
+        ];
       };
-      scrapeConfigs = [
-        {
-          job_name = "nodes";
-          static_configs = [
-            {
-              targets = [
-                "127.0.0.1:${toString config.services.prometheus.exporters.node.port}"
-              ];
-            }
-          ];
-        }
-      ];
+      services.caddy = {
+        enable = true;
+        virtualHosts."prometheus.${domain}".extraConfig = ''
+          reverse_proxy http://127.0.0.1:${builtins.toString config.services.prometheus.port}        
+        '';
+      };
     };
-  };
 }
-#  services.nginx = {
-#     enable = true;
-#     recommendedProxySettings = true;
-#     recommendedOptimisation = true;
-#     recommendedGzipSettings = true;
-#     # recommendedTlsSettings = true;
-
-#     upstreams = {
-#       "grafana" = {
-#         servers = {
-#           "127.0.0.1:${toString config.services.grafana.port}" = {};
-#         };
-#       };
-#       "prometheus" = {
-#         servers = {
-#           "127.0.0.1:${toString config.services.prometheus.port}" = {};
-#         };
-#       };
-#     };
-
-#     virtualHosts ={
-#       grafana = {
-#       locations."/" = {
-#         proxyPass = "http://grafana";
-#         proxyWebsockets = true;
-#       };
-#       listen = [{
-#         addr = "192.168.1.10";
-#         port = 8010;
-#       }];
-#       };
-#       prometheus = {
-#         locations."/".proxyPass = "http://prometheus";
-#         listen = [{
-#           addr = "192.168.1.10";
-#           port = 8020;
-#         }];
-#       };
-#     };
-#   };
