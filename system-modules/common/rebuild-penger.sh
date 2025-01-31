3c2c332282be3756dd26ecbdd4690992ebcc3100
blockdoth <pepijn.pve@gmail.com>
2025-01-31 19:35:48 +0100

    [nuc@penger] (2025-01-31 19:12) System Generation 169

--- system-modules/common/rebuild-penger.sh
+++ system-modules/common/rebuild-penger.sh
@@ -11,9 +11,9 @@ git reset > /dev/null
 git add ./hosts/* ./system-modules/* ./assets/* ./flake.nix ./flake.lock ./.gitignore ./.sops.yaml ./secrets/*
 git --no-pager diff -U0 --staged .
 
-NUC_IP=$(tailscale status | rg nuc | awk '{print $1}')
+NUC_IP=$(tailscale status | rg "$HOSTNAME" | awk '{print $1}')
 
-sudo nixos-rebuild --target-host penger@"$NUC_IP" --use-remote-sudo switch --flake .#nuc
+sudo nixos-rebuild --target-host "$USERNAME"@"$NUC_IP" --use-remote-sudo switch --flake .#"$HOSTNAME"
 COMMIT_MESSAGE=$(nixos-rebuild list-generations | sed -n '2p' | awk -v host="$HOSTNAME" -v user="$USERNAME" '{printf "[%s@%s] (%s %s) System Generation %s\n", host, user, $3, substr($4, 0, 5), $1}')
 
 git commit -m "$COMMIT_MESSAGE"
