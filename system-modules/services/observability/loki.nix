fc2d558c564f1a19a0da33cd19749dd515afe931
blockdoth <pepijn.pve@gmail.com>
2025-02-03 21:28:25 +0100

    [desktop@blockdoth] (2025-02-03 21:28) System Generation 189

--- system-modules/services/observability/loki.nix
+++ system-modules/services/observability/loki.nix
@@ -9,20 +9,15 @@
   options = {
     system-modules.services.loki = {
       enable = lib.mkEnableOption "Enables loki";
-      port = lib.mkOption { type = lib.types.str; };
+      # port = lib.mkOption { type = lib.types.str; };
     };
   };
 
   config = lib.mkIf config.system-modules.services.loki.enable {
-    system-modules.services.loki.port = "3100";
+    # system-modules.services.loki.port = "3100";
     services.loki = {
       enable = true;
-      configuration = {
-        auth_enabled = true;
-      };
-      extraFlags = [
-        "--server.http-listen-port=${config.system-modules.services.loki.port}"
-      ];
+      configFile = ./loki.yaml;
     };
   };
 }
--- system-modules/services/observability/promtail.nix
+++ system-modules/services/observability/promtail.nix
@@ -13,34 +13,7 @@
   config = lib.mkIf config.system-modules.services.promtail.enable {
     services.promtail = {
       enable = true;
-      configuration = {
-        server = {
-          http_listen_port = 9080;
-          grpc_listen_port = 0;
-
-        };
-        positions = {
-          filename = "/tmp/positions.yaml";
-        };
-        clients = {
-          url = "http://localhost:${config.system-modules.services.loki.port}/loki/api/v1/push";
-        };
-
-        scrape_configs = [
-          {
-            job_name = "logs";
-            static_configs = [
-              {
-                targets = [ "localhost" ];
-                labels = {
-                  job = "varlogs";
-                  __path__ = "/var/log/pisslog.*";
-                };
-              }
-            ];
-          }
-        ];
-      };
+      configFile = ./promtail.yaml;
     };
   };
 }
