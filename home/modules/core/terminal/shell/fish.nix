a34959b1049da76c367517cd2fbca93a398827f6
blockdoth <pepijn.pve@gmail.com>
2025-02-09 17:14:23 +0100

    [desktop@blockdoth] (2025-02-09 17:14) Home Generation 875

--- home/modules/core/terminal/shell/fish.nix
+++ home/modules/core/terminal/shell/fish.nix
@@ -20,20 +20,20 @@
       shellAliases = lib.mkMerge [
         {
           cls = "clear";
-          conf = "cd /home/${config.home.username}/nixos";
-          repos = "cd /home/${config.home.username}/Documents/repos";
+          conf = "cd ~/nixos";
+          repos = "cd ~/repos";
           btm = "btm --process_memory_as_value -g";
           mkscript = "echo '#!/usr/bin/env bash' > script-template.sh && chmod +x script-template.sh";
           log = "git log --graph --pretty=format:'%C(bold red)%h%Creset - %C(bold blue)%an%Creset%C(auto)%d%Creset %s %C(yellow)%ad%Creset %Cgreen(%cr) ' --abbrev-commit --date=human --decorate=full --all";
           penger = "ssh penger@nuc";
           laptop = "ssh blockdoth@laptop";
           desktop = "ssh blockdoth@desktop";
-          temp = "cd ~/Documents/temp/";
+          temp = "cd ~/temp/";
           eep = "systemctl suspend";
         }
         (lib.mkIf (config.home.username != "penger") {
           # Graphical
-          config = "confh && codium .";
+          config = "conf && codium .";
           lock = "kill hyprlock && hyprlock";
           print = "firefox -new-tab https://printportal.tudelft.nl:9443/end-user/ui/dashboard";
         })
