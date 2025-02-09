6a125d64d1d991443b223fa1f55bf907315c3272
blockdoth <pepijn.pve@gmail.com>
2025-02-09 15:05:14 +0100

    [nuc@penger] (2025-02-09 15:05) System Generation 48

--- system-modules/services/syncthing.nix
+++ system-modules/services/syncthing.nix
@@ -24,6 +24,10 @@
             urAccepted = -1;
             relaysEnabled = false;
           };
+          guiAddress = "127.0.0.1:8384"; # Web UI address
+          insecureSkipHostcheck = true;
+          guiTheme = "default"; # Default theme
+
           devices = {
             "nuc" = {
               id = "";
@@ -68,10 +72,10 @@
       # Disables a default sync folder from being created
       systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
 
-      # services.caddy = {
-      #   virtualHosts."syncthing.${domain}".extraConfig = ''
-      #     reverse_proxy ${config.services.syncthing.guiAddress}
-      #   '';
-      # };
+      services.caddy = {
+        virtualHosts."syncthing.${domain}".extraConfig = ''
+          reverse_proxy ${config.services.syncthing.settings.guiAddress}
+        '';
+      };
     };
 }
