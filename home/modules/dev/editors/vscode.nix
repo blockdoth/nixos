93070ad0953b0837ac050daee906ad3d986c2309
blockdoth <pepijn.pve@gmail.com>
2025-02-11 11:22:31 +0100

    [laptop@blockdoth] (2025-02-11 11:22) Home Generation 394

--- home/modules/dev/editors/vscode.nix
+++ home/modules/dev/editors/vscode.nix
@@ -32,6 +32,8 @@
         terminal.integrated.shellIntegration.enabled = false;
         # clangd.arguments= ["--compile-commands-dir=/home/blockdoth/Documents/repos/c-web-server"];
         clangd.path = "/home/blockdoth/.config/VSCodium/User/globalStorage/llvm-vs-code-extensions.vscode-clangd/install/18.1.3/clangd_18.1.3/bin/clangd";
+        haskell.manageHLS = "GHCup";
+
       };
     };
   };
