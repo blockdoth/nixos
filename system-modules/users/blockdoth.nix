c348c70694e089ae3d6e684661214d22b86fdf74
blockdoth <pepijn.pve@gmail.com>
2025-02-18 13:40:38 +0100

    [laptop@blockdoth] (2025-02-18 13:40) System Generation 100

--- system-modules/users/blockdoth.nix
+++ system-modules/users/blockdoth.nix
@@ -32,8 +32,7 @@
             keys = [
               (builtins.readFile ../../hosts/desktop/id_ed25519.pub)
               (builtins.readFile ../../hosts/laptop/id_ed25519.pub)
-              # phone
-              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpzvsxZud2IPq04WtljDQdmOSHukwEncFupWzeGblmZ2diZ5NBcaNbGULKtcapIk6F2sbFDjoiwU8Zj0bdSmYxLUnweQCsvYgqVFj9reIWMZdK+GNSDaaTPU9ay6es45p8O5OIvq79OqBUatTpj6E/Mn3K+zibNWfuwysWw28SxOsva2SmN2SOKq4vi5gDjMT+qnl2wxy70pDux17/ec55Jz1XVGKhPhS1UUofvp4qunst/3qcqNywLS0JkQD5MTlVZzGKEayNSvFhwYvQqPe3+yX3pMikhJx+hUgEPjK5lu0tlXBJkk5BJxcX1azfQrAvd29MegNYPCE3MZm/QlBNwoGVMigZ/uCED1BUccz8z1YAjt9S+RrI9phOdV1MLvH3jQDYWG/w5YNVT6iyJOiSVFYeKhmP0mxZ9ffyoYdefsUazYWDIpKKWMQAA9NMrXzjdMbE729WmoFgiVeHrnW/IgabSQlpz1ZQpiD/gQ6u0W2db2NjFEkE5kSB5/4n0jk= u0_a345@localhost"
+              (builtins.readFile ../../hosts/phone/id_ed25519.pub)
             ];
           };
         };
