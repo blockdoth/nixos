7c7d84aa8b0a1a9526303288f5bc5114750ccee8
blockdoth <pepijn.pve@gmail.com>
2025-02-21 12:00:07 +0100

    [laptop@blockdoth] (2025-02-21 12:00) Home Generation 407

--- home/modules/core/windowmanager/tiling/lockscreen/hyprlock.nix
+++ home/modules/core/windowmanager/tiling/lockscreen/hyprlock.nix
@@ -62,7 +62,7 @@
             text = "cmd[update:10] echo \"$(date +\"%-I:%M%p\")\"";
             # color = "#cdd6f4";
             color = "rgba(255, 255, 255, 0.6)";
-            font_size = 400;
+            font_size = 350;
             font_family = "JetBrains Mono Ultra-Bold";
             position = "0, -200";
             halign = "center";
