e3c5c41b0c1d4dbb0f919d25a38614fbe2c886ae
blockdoth <pepijn.pve@gmail.com>
2025-03-07 13:51:25 +0100

    [laptop@blockdoth] (2025-03-07 13:51) Home Generation 426

--- home/modules/core/windowmanager/tiling/hyprland/windowrules.nix
+++ home/modules/core/windowmanager/tiling/hyprland/windowrules.nix
@@ -8,7 +8,7 @@
         "blur, waybar"
       ];
 
-      "$scratchpad" = "class:^(scratchpad|spotify|com.rtosta.zapzap)";
+      "$scratchpad" = "class:^(scratchpad|spotify|Spotify|com.rtosta.zapzap)";
       "$pip" = "title:^(Picture-in-Picture)";
       "$popup" = "class:^(org.pulseaudio.pavucontrol|.blueman-manager-wrapped|Matplotlib)";
 
@@ -16,7 +16,7 @@
         #transpancy
         "opacity 0.85, class:^(firefox)"
         "opacity 1.00, title:^(.*YouTube.*)"
-        "opacity 0.75, class:^(spotify)"
+        "opacity 0.75, class:^(spotify|Spotify)"
         "opacity 0.85, class:^(VSCodium|codium)"
         "opacity 0.80, class:^(vesktop)"
         "opacity 0.85, class:^(jetbrains)"
