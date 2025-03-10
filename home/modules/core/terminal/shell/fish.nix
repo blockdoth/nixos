44602af07511333ac739677d2bc69cd531d563af
blockdoth <pepijn.pve@gmail.com>
2025-03-10 14:02:44 +0100

    [laptop@blockdoth] (2025-03-10 14:02) Home Generation 436

--- home/modules/core/terminal/shell/fish.nix
+++ home/modules/core/terminal/shell/fish.nix
@@ -26,9 +26,9 @@
           btm = "btm --process_memory_as_value -g";
           mkscript = "echo '#!/usr/bin/env bash' > script-template.sh && chmod +x script-template.sh";
           log = "git log --graph --pretty=format:'%C(bold red)%h%Creset - %C(bold blue)%an%Creset%C(auto)%d%Creset %s %C(yellow)%ad%Creset %Cgreen(%cr) ' --abbrev-commit --date=human --decorate=full --all";
-          penger = "ssh penger@nuc";
-          laptop = "ssh blockdoth@laptop";
-          desktop = "ssh blockdoth@desktop";
+          penger = "ssh penger@nuc -t \"fish\"";
+          laptop = "ssh blockdoth@laptop -t \"fish\"";
+          desktop = "ssh blockdoth@desktop -t \"fish\"";
           temp = "cd ~/temp/";
           up = "cd ..";
           eep = "systemctl suspend";
