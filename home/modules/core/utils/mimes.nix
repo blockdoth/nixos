ca031b92ee470c86581f158ba70113d354d8a9f4
blockdoth <pepijn.pve@gmail.com>
2025-03-11 22:37:53 +0100

    [laptop@blockdoth] (2025-03-11 22:37) Home Generation 446

--- home/modules/core/utils/mimes.nix
+++ home/modules/core/utils/mimes.nix
@@ -24,14 +24,12 @@ in
         micro-terminal = {
           name = "Micro Terminal";
           exec = "alacritty -e micro %f";
-          terminal = true;
           type = "Application";
           mimeType = [ "application/txt" ];
         };
         micro-terminal-json = {
           name = "Micro Terminal For Json";
           exec = "alacritty -e sh -c \"jq . \\\"%f\\\" | micro -\"";
-          terminal = true;
           type = "Application";
           mimeType = [ "application/json" ];
         };
