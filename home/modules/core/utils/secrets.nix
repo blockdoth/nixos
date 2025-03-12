7f1f3d53c7037f072892dc658278e6a9ee090187
blockdoth <pepijn.pve@gmail.com>
2025-03-12 13:56:42 +0100

    [laptop@blockdoth] (2025-03-12 13:56) Home Generation 447

--- home/modules/core/utils/secrets.nix
+++ home/modules/core/utils/secrets.nix
@@ -6,6 +6,7 @@
 }:
 let
   module = config.modules.core.utils.secrets;
+  keysFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 in
 {
   imports = [ inputs.sops-nix.homeManagerModules.sops ];
