ed99fd6de001a9d98d2dbefd1c2dfc0ec5786a49
blockdoth <pepijn.pve@gmail.com>
2025-03-12 14:01:09 +0100

    [laptop@blockdoth] (2025-03-12 14:01) System Generation 121

--- system-modules/services/network/authelia.nix
+++ system-modules/services/network/authelia.nix
@@ -6,16 +6,31 @@
   ...
 }:
 let
-  module = config.system-modules.services.network.acme;
+  module = config.system-modules.services.network.authelia;
   domain = config.system-modules.services.network.domains.homelab;
   autheliaPort = 9091;
 in
 {
   config = lib.mkIf module.enable {
+    sops.secrets = {
+      authelia-jwt = { };
+      authelia-storage-encryption = { };
+    };
+
     services.authelia.instances.main = {
       enable = true;
+      secrets = {
+        jwtSecretFile = config.sops.secrets.authelia-jwt.path;
+        storageEncryptionKeyFile = config.sops.secrets.authelia-storage-encryption.path;
+      };
       settings = {
-        server.address = "http://127.0.0.1:${autheliaPort}";
+        server = {
+          address = "http://127.0.0.1:${builtins.toString autheliaPort}";
+
+          # Necessary for Caddy integration
+          # See https://www.authelia.com/integration/proxies/caddy/#implementation
+          endpoints.authz.forward-auth.implementation = "ForwardAuth";
+        };
       };
     };
 
