e6cc533891a75be890ab8ccbc117fd9245eefe37
blockdoth <pepijn.pve@gmail.com>
2025-03-13 10:54:54 +0100

    [desktop@blockdoth] (2025-03-13 10:54) Home Generation 894

--- home/modules/dev/editors/vscode.nix
+++ home/modules/dev/editors/vscode.nix
@@ -14,26 +14,28 @@ in
     programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
-      extensions = with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide
-        llvm-vs-code-extensions.vscode-clangd
-        mkhl.direnv
-      ];
-      userSettings = {
-        files.autoSave = "afterDelay";
-        editor.tabSize = 2;
-        workbench.tree.indent = 15;
-        editor.fontSize = 16;
-        editor.detectIndentation = false;
-        redhat.telemetry.enabled = false;
-        workbench.colorTheme = "Stylix";
-        window.title = "VSCodium \${activeRepositoryName}";
-        terminal.integrated.shellIntegration.enabled = false;
-        # clangd.arguments= ["--compile-commands-dir=/home/blockdoth/Documents/repos/c-web-server"];
-        clangd.path = "/home/blockdoth/.config/VSCodium/User/globalStorage/llvm-vs-code-extensions.vscode-clangd/install/18.1.3/clangd_18.1.3/bin/clangd";
-        haskell.manageHLS = "GHCup";
-        jupyter.notebookFileRoot = "\${fileDirname}";
-        jupyter.askForKernelRestart = "false";
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          jnoortheen.nix-ide
+          llvm-vs-code-extensions.vscode-clangd
+          mkhl.direnv
+        ];
+        userSettings = {
+          files.autoSave = "afterDelay";
+          editor.tabSize = 2;
+          workbench.tree.indent = 15;
+          editor.fontSize = 16;
+          editor.detectIndentation = false;
+          redhat.telemetry.enabled = false;
+          workbench.colorTheme = "Stylix";
+          window.title = "VSCodium \${activeRepositoryName}";
+          terminal.integrated.shellIntegration.enabled = false;
+          # clangd.arguments= ["--compile-commands-dir=/home/blockdoth/Documents/repos/c-web-server"];
+          clangd.path = "/home/blockdoth/.config/VSCodium/User/globalStorage/llvm-vs-code-extensions.vscode-clangd/install/18.1.3/clangd_18.1.3/bin/clangd";
+          haskell.manageHLS = "GHCup";
+          jupyter.notebookFileRoot = "\${fileDirname}";
+          jupyter.askForKernelRestart = "false";
+        };
       };
     };
   };
