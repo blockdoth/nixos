e4bafc0f1b8b2dc705b3a9d7c1661f715315704b
blockdoth <pepijn.pve@gmail.com>
2025-03-17 11:03:16 +0100

    [laptop@blockdoth] (2025-03-17 11:03) System Generation 129

---
+++ readme.md
@@ -0,0 +1,14 @@
+# My Config
+The best thing since sliced bread
+
+### TODO
+- Single Sign on for all services with authelia and lldap
+- Blocky or an alternative ad blocker
+- Finish headscale user management 
+- Finish setting up media services
+- Finish setting up piss stream observability
+
+### Future plans
+- Impermanence with btrfs and disko 
+- Linkwarden once it gets merged (maybe build from pr)
+- File browser service once it gets merged (maybe build from pr)
--- system-modules/core/rebuild.sh
+++ system-modules/core/rebuild.sh
@@ -15,12 +15,12 @@ nixfmt .
 
 git reset > /dev/null 
 if [ "$TYPE" = "system" ]; then
-  git add ./hosts/* ./system-modules/* ./assets/* ./flake.nix ./flake.lock ./.gitignore ./.sops.yaml ./secrets/*
+  git add ./hosts/* ./system-modules/* ./assets/* ./flake.nix ./flake.lock ./.gitignore ./.sops.yaml ./secrets/* ./readme.md
   git --no-pager diff -U0 --staged .
   nh os switch --hostname "$HOSTNAME" . > /dev/null 
   COMMIT_MESSAGE=$(nixos-rebuild list-generations | sed -n '2p' | awk -v host="$HOSTNAME" -v user="$USERNAME" '{printf "[%s@%s] (%s %s) System Generation %s\n", host, user, $3, substr($4, 0, 5), $1}')
 elif [ "$TYPE" = "home" ]; then
-  git add ./home/* ./assets/* ./flake.nix ./flake.lock ./.gitignore ./.sops.yaml ./secrets/*
+  git add ./home/* ./assets/* ./flake.nix ./flake.lock ./.gitignore ./.sops.yaml ./secrets/* ./readme.md
   git --no-pager diff -U0 --staged .
   nh home switch --configuration "$HOSTNAME-$USERNAME" .
   home-manager generations > gen.txt
