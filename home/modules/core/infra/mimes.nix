8a5b55410c32763eb7a0382371466867d1b6e3a8
blockdoth <pepijn.pve@gmail.com>
2025-03-22 16:38:03 +0100

    [laptop@blockdoth] (2025-03-22 16:38) Home Generation 474

--- home/modules/core/infra/mimes.nix
+++ home/modules/core/infra/mimes.nix
@@ -17,6 +17,12 @@ in
           "application/json" = [ "micro-terminal-json.desktop" ];
           "application/txt" = [ "micro-terminal.desktop" ];
           "x-scheme-handler/discord" = [ "vesktop.desktop" ];
+          "default-web-browser" = [ "firefox.desktop" ];
+          "text/html" = [ "firefox.desktop" ];
+          "x-scheme-handler/http" = [ "firefox.desktop" ];
+          "x-scheme-handler/https" = [ "firefox.desktop" ];
+          "x-scheme-handler/about" = [ "firefox.desktop" ];
+          "x-scheme-handler/unknown" = [ "firefox.desktop" ];
         };
       };
 
