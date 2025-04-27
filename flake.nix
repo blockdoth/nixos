1fe7342f444b79e1d1eff9338501117dccdf8f05
blockdoth <pepijn.pve@gmail.com>
2025-04-27 22:01:20 +0200

    [desktop@blockdoth] (2025-04-27 15:34) System Generation 243

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
   outputs =
@@ -54,6 +55,18 @@
         inherit system;
         config.allowUnfree = true;
         # crossSystem = { config = "aarch64-linux"; };  # Enable cross-compilation
+      };
+      deployPkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          inputs.deploy-rs.overlay # or deploy-rs.overlays.default
+          (self: super: {
+            inputs.deploy-rs = {
+              inherit (pkgs) deploy-rs;
+              lib = super.deploy-rs.lib;
+            };
+          })
+        ];
       };
     in
     {
@@ -135,5 +148,20 @@
           ];
         };
       };
+
+      deploy = {
+        nodes.nuc = {
+          hostname = "nuc";
+          interactiveSudo = true;
+          profiles.system = {
+            user = "root";
+            path = deployPkgs.deploy-rs.lib.activate.nixos self.nixosConfigurations.nuc;
+          };
+        };
+      };
+
+      # This is highly advised, and will prevent many possible mistakes
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deployPkgs.lib;
+
     };
 }
