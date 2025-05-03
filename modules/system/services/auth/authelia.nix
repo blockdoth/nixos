bdad06b5bd0e75aa16b80d4fb93b94707c36a38c
blockdoth <pepijn.pve@gmail.com>
2025-05-03 19:30:13 +0200

    [nuc@penger] (2025-05-03 19:30) System Generation 130

--- modules/system/services/auth/authelia.nix
+++ modules/system/services/auth/authelia.nix
@@ -107,39 +107,23 @@ in
         storage.local.path = "/var/lib/authelia-main/db.sqlite3";
 
         identity_providers.oidc = {
-          jwks = [
-            {
-              kid = "dev-key";
-              use = "sig";
-              alg = "RS256";
-              kty = "RSA";
-              n = "wTj3dA9wnC7PuyBlBPwN7HNB0X2k6WaD2cLQm1TC7a3JeugK1T6oIoByJBOxSvU0H_o6zv7LAd7VRhk4gM7NNXrmZkaEFxR88l9ckF5J9h9vJBYwWCKrYldZhv6NH8szH0Vq1Zz2h_BF9vRjIOkOVLg94VdRUh6Toq5C13dV6gM";
-              e = "AQAB";
-              d = "JuHeN-lbSTy4q1fB4BCb1_xVdo7MZlWj_hl4DwKu_x0bq0IgFJHa7yDWgcPZmkmMLm1Pfczh_Gc9t0QpmZp1M5M8rs6LzsyBl7kImgbOgClqshvboUnm1LwKm9a4Xooxs_EeqZ1eKboUHVNzvGm9Q70jJG6wbK8G1H6h9Fk-ViE";
-              p = "9P9mC6n25ohsPt29dC6cf5B1gUQd3kKz6JbC-uD4fXA";
-              q = "wRWaSYiU2FY8Wm_pn2zFhEKhc_h97Tb7I3aHgeHUEQE";
-              dp = "jzGSEs6iTT_Up__Vct9MNZGkrIfp6xMIaS6T-6vb3iE";
-              dq = "gUQuqD8JPJmGeUbcYuBwhDk-NNxFKaXrXdcR0ruBSAE";
-              qi = "ggQ-vEcMWj13y3oKYzdwVnRrHFn7W8v7mIdNvAYc6pA";
-            }
-          ];
           clients = [
-            # {
-            #   authorization_policy = "one_factor";
-            #   client_id = "immich";
-            #   client_secret = "";
-            #   redirect_uris = [
-            #     "https://immich.${domain}/auth/login"
-            #     "https://immich.${domain}/user-settings"
-            #     "app.immich:///oauth-callback"
-            #   ];
-            #   scopes = [
-            #     "openid"
-            #     "profile"
-            #     "email"
-            #   ];
-            #   userinfo_signed_response_alg = "none";
-            # }
+            {
+              authorization_policy = "one_factor";
+              client_id = "immich";
+              client_secret = "XsdEXVthp2NYx67va6Cy0X4/oJGU7koBbDsWjxqsfjM=";
+              redirect_uris = [
+                "https://immich.${domain}/auth/login"
+                "https://immich.${domain}/user-settings"
+                "app.immich:///oauth-callback"
+              ];
+              scopes = [
+                "openid"
+                "profile"
+                "email"
+              ];
+              userinfo_signed_response_alg = "none";
+            }
           ];
         };
       };
