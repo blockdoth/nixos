be17043febf78f8a500fe4a74a555e056aece7f5
blockdoth <pepijn.pve@gmail.com>
2025-05-03 21:50:16 +0200

    [nuc@penger] (2025-05-03 21:50) System Generation 143

--- modules/system/services/auth/lldap.nix
+++ modules/system/services/auth/lldap.nix
@@ -68,9 +68,9 @@ in
 
     };
 
-    services.caddy.virtualHosts."lldap.${domain}".extraConfig = ''
-      reverse_proxy 127.0.0.1:${builtins.toString config.services.lldap.settings.http_port}        
-    '';
+    # services.caddy.virtualHosts."lldap.${domain}".extraConfig = ''
+    #   reverse_proxy 127.0.0.1:${builtins.toString config.services.lldap.settings.http_port}
+    # '';
 
     system-modules.services.observability.gatus.endpoints = [
       {
