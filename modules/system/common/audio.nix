2a622829b38de1621a9390062cdf3f9d7c3a82b4
blockdoth <pepijn.pve@gmail.com>
2025-05-25 19:12:58 +0200

    [desktop@blockdoth] (2025-05-25 19:12) System Generation 247

--- modules/system/common/audio.nix
+++ modules/system/common/audio.nix
@@ -4,33 +4,29 @@ let
 in
 {
   config = lib.mkIf module.enable {
-    security.rtkit.enable = true;
+    # security.rtkit.enable = true;
 
     # all disabled since it doesnt work
-    # services.pipewire = {
-    #   # enable = true;
-    #   alsa = {
-    #     enable = true;
-    #     support32Bit = true;
-    #   };
-    #   wireplumber.enable = true;
+    services.pipewire = {
+      # enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      wireplumber.enable = true;
 
-    #   pulse.enable = true;
-    #   jack.enable = true;
-    # };
+      pulse.enable = true;
+      jack.enable = true;
+    };
 
     # only pulse audio seems to work, not pipewire
     hardware = {
       enableAllFirmware = true;
     };
-    services.pulseaudio = {
-      enable = false;
-      support32Bit = true;
-    };
+    # services.pulseaudio = {
+    #   enable = false;
+    #   support32Bit = true;
+    # };
 
-    # triggers a full local rebuild if set to false
-    nixpkgs.config = {
-      pulseaudio = true;
-    };
   };
 }
