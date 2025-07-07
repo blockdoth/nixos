1b46dc2a032fb9b17c221b18f6c75992891b1d43
blockdoth <pepijn.pve@gmail.com>
2025-07-07 18:43:44 +0200

    [desktop@blockdoth] (2025-07-07 18:43) Home Generation 949

--- modules/home/core/windowmanager/tiling/logout/wlogout.sh
+++ modules/home/core/windowmanager/tiling/logout/wlogout.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
-LOCK_FILE="/tmp/wlogout.lock"
-
-if [ -e "$LOCK_FILE" ]; then
+# Check if wlogout is already running
+if pidof wlogout >/dev/null; then
     exit 0
 fi
 
-(
-  set -C; # Prevents overwritting
-  echo "$$" > "$LOCK_FILE"
-) 2>/dev/null
-
 wlogout -b 5
 
-rm -f "$LOCK_FILE"
