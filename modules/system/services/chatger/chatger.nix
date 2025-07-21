6725a6eaa6bfb8a592e909c041a04bf1450bf247
blockdoth <pepijn.pve@gmail.com>
2025-07-21 21:50:51 +0200

    [desktop@blockdoth] (21:50:46 25.11) System Generation 260

--- modules/system/services/chatger/chatger.nix
+++ modules/system/services/chatger/chatger.nix
@@ -9,15 +9,14 @@ let
   module = config.system-modules.services.chatger;
   domain = config.system-modules.services.network.domains.homelab;
   chatger = pkgs.stdenv.mkDerivation {
-    name = "fossil-example";
-
+    name = "chatger";
     src = pkgs.fetchFromGitHub {
       owner = "blockdoth";
       repo = "chatger-fossil-mirror";
       rev = "main";
       sha256 = "sha256-FMmvoesIiJ0hevMbADNQf8sVvxNTzzKhykCyk6vblX0=";
     };
-    patches = [ ./db_path.patch ];
+    patches = [ ./db_from_env.patch ];
     buildPhase = ''
       cc -o nob nob.c
       ./nob
@@ -25,9 +24,6 @@ let
 
     installPhase = ''
       mkdir -p $out/bin
-      mkdir -p $out/bin/sql
-
-      cp -r ./sql/ $out/bin/sql
       cp ./chatger $out/bin/
     '';
   };
@@ -44,8 +40,9 @@ in
       wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
-        ExecStart = "/home/penger/sources/chatger/chatger";
-        WorkingDirectory = "/home/penger/sources";
+        ExecStart = "${chatger}/bin/chatger";
+        StateDirectory = "chatger";
+        Environment = "CHATGER_DB_PATH=/var/lib/chatger/chatger.db";
         Restart = "on-failure";
         User = "penger";
         Group = "penger";
