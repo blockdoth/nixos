6f7add03227343404701b282a9e7c3990811edb2
blockdoth <pepijn.pve@gmail.com>
2025-07-21 14:23:25 +0200

    [desktop@blockdoth] (16:33:01 25.11) System Generation 257

--- modules/system/common/power.nix
+++ modules/system/common/power.nix
@@ -7,7 +7,20 @@ in
     services = {
       upower.enable = true;
       power-profiles-daemon.enable = false;
-      auto-cpufreq.enable = true;
+      thermald.enable = true;
+      auto-cpufreq = {
+        enable = true;
+        settings = {
+          battery = {
+            governor = "powersave";
+            turbo = "never";
+          };
+          charger = {
+            governor = "powersave";
+            turbo = "never";
+          };
+        };
+      };
     };
   };
 }
