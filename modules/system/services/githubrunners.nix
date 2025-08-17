a9d9221930f819259d6a67d85c6677fa6e38c52e
blockdoth <pepijn.pve@gmail.com>
2025-08-18 00:15:27 +0200

    [desktop@blockdoth] (00:15:17 25.11) System Generation 278

--- modules/system/services/githubrunners.nix
+++ modules/system/services/githubrunners.nix
@@ -11,14 +11,23 @@ let
 in
 {
   config = lib.mkIf module.enable {
-    sops.secrets.github-runner-token = { };
+    sops.secrets = {
+      githubrunner-chatger-tui-1-token = { };
+      githubrunner-chatger-tui-2-token = { };
+    };
 
     services.github-runners = {
-      chatgertui-runner = {
+      chatgertui-runner-1 = {
         enable = true;
-        name = "chatgertui-runner";
+        name = "chatgertui runner 1";
         url = "https://github.com/blockdoth/chatger-tui";
-        tokenFile = config.sops.secrets.github-runner-token.path;
+        tokenFile = config.sops.secrets.githubrunner-chatger-tui-1-token.path;
+      };
+      chatgertui-runner-2 = {
+        enable = true;
+        name = "chatgertui runner 2";
+        url = "https://github.com/blockdoth/chatger-tui";
+        tokenFile = config.sops.secrets.githubrunner-chatger-tui-2-token.path;
       };
     };
   };
