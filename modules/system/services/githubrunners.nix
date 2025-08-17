75220c54d9abb8d457abc7f7cd8f69b43e27581f
blockdoth <pepijn.pve@gmail.com>
2025-08-18 00:46:06 +0200

    [desktop@blockdoth] (00:46:02 25.11) System Generation 279

--- modules/system/services/githubrunners.nix
+++ modules/system/services/githubrunners.nix
@@ -12,22 +12,39 @@ in
 {
   config = lib.mkIf module.enable {
     sops.secrets = {
-      githubrunner-chatger-tui-1-token = { };
-      githubrunner-chatger-tui-2-token = { };
+      githubrunner-trusted-1-token = { };
+      githubrunner-trusted-2-token = { };
+      githubrunner-untrusted-1-token = { };
+    };
+
+    users.groups.githubrunners = { };
+    users.users.githubrunners = {
+      isSystemUser = true;
+      group = "githubrunners";
+      description = "GitHub Actions Runner User";
+      home = "/var/lib/githubrunners";
+      createHome = true;
+      shell = pkgs.bash;
     };
 
     services.github-runners = {
-      chatgertui-runner-1 = {
+      chatgertui-trusted-1 = {
         enable = true;
-        name = "chatgertui runner 1";
+        name = "trusted runner 1";
         url = "https://github.com/blockdoth/chatger-tui";
-        tokenFile = config.sops.secrets.githubrunner-chatger-tui-1-token.path;
+        tokenFile = config.sops.secrets.githubrunner-trusted-1-token.path;
       };
-      chatgertui-runner-2 = {
+      chatgertui-trusted-2 = {
         enable = true;
-        name = "chatgertui runner 2";
+        name = "trusted runner 2";
         url = "https://github.com/blockdoth/chatger-tui";
-        tokenFile = config.sops.secrets.githubrunner-chatger-tui-2-token.path;
+        tokenFile = config.sops.secrets.githubrunner-trusted-2-token.path;
+      };
+      chatgertui-untrusted-1 = {
+        enable = true;
+        name = "untrusted runner 1";
+        url = "https://github.com/blockdoth/chatger-tui";
+        tokenFile = config.sops.secrets.githubrunner-untrusted-1-token.path;
       };
     };
   };
