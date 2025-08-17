ca65590bee206b8d0d5480b29595e5a186c31bf2
blockdoth <pepijn.pve@gmail.com>
2025-08-17 23:25:02 +0200

    [desktop@blockdoth] (23:24:57 25.11) System Generation 276

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -24,6 +24,7 @@
       syncthing.enable = true;
       bluetooth.enable = true;
       docker.enable = true;
+      crosscompilation.enable = true;
     };
     services = {
       # scraping.connectbox.enable = true;
