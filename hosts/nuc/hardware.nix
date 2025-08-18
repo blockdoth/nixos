a1661c24c5dddbb777d019252d86a9743f53d019
blockdoth <pepijn.pve@gmail.com>
2025-08-18 22:28:22 +0200

    [nuc@penger] (22:27:23 25.11) System Generation 285

--- hosts/nuc/hardware.nix
+++ hosts/nuc/hardware.nix
@@ -45,7 +45,12 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 8 * 1024; # 8GB
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
