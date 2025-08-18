8c1e23294632354b066b491fc1b90ff5d420ae2d
blockdoth <pepijn.pve@gmail.com>
2025-08-18 02:18:15 +0200

    [nuc@penger] (02:18:01 25.11) System Generation 283

--- modules/system/services/githubrunners.nix
+++ modules/system/services/githubrunners.nix
@@ -11,9 +11,12 @@ let
 in
 {
   config = lib.mkIf module.enable {
+<<<<<<< HEAD
     environment.systemPackages = [
       pkgs.xz # For decompressing cache between runs
     ];
+=======
+>>>>>>> 9982bf3 ([nuc@penger] (02:18:01 25.11) System Generation 283)
 
     sops.secrets = {
       githubrunner-chatger-tui-token = { };
@@ -33,9 +36,23 @@ in
         enable = true;
         name = "chatger-tui runner";
         url = "https://github.com/blockdoth/chatger-tui";
+<<<<<<< HEAD
         tokenFile = config.sops.secrets.githubrunner-chatger-tui-token.path;
+=======
+        tokenFile = config.sops.secrets.githubrunner-1-token.path;
         user = "githubrunners";
         group = "githubrunners";
+        extraPackages = [ pkgs.xz ];
+      };
+      chatgertui-runner-2 = {
+        enable = true;
+        name = "runner 2";
+        url = "https://github.com/blockdoth/chatger-tui";
+        tokenFile = config.sops.secrets.githubrunner-2-token.path;
+>>>>>>> 9982bf3 ([nuc@penger] (02:18:01 25.11) System Generation 283)
+        user = "githubrunners";
+        group = "githubrunners";
+        extraPackages = [ pkgs.xz ];
       };
     };
   };
