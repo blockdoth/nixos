2c1674695896616eb20769ddc1c645d1193297e0
blockdoth <pepijn.pve@gmail.com>
2025-08-22 18:58:31 +0200

    [desktop@blockdoth] (2025-08-22 18:58) Home Generation 962

--- modules/home/dev/editors/vscode.nix
+++ modules/home/dev/editors/vscode.nix
@@ -9,7 +9,10 @@ let
 in
 {
   config = lib.mkIf module.enable {
-    home.packages = with pkgs; [ vscodium ];
+    home.packages = with pkgs; [
+      vscodium
+      nixd
+    ];
 
     programs.vscode = {
       enable = true;
@@ -37,14 +40,15 @@ in
           window.customTitleBarVisibility = "never";
           # clangd.arguments= ["--compile-commands-dir=/home/blockdoth/Documents/repos/c-web-server"];
           clangd.path = "/home/blockdoth/.config/VSCodium/User/globalStorage/llvm-vs-code-extensions.vscode-clangd/install/18.1.3/clangd_18.1.3/bin/clangd";
-          haskell.manageHLS = "GHCup";
           jupyter.notebookFileRoot = "\${fileDirname}";
           jupyter.askForKernelRestart = "false";
           rust-analyzer.server.path = "rust-analyzer";
           vscode-pets.petSize = "medium";
           vscode-pets.throwBallWithMouse = true;
           git.blame.editorDecoration.enabled = true;
-
+          nix.enableLanguageServer = true;
+          nix.serverPath = "nixd";
+          haskell.manageHLS = "PATH";
         };
       };
     };
