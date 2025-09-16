f0172476a928360c5d9391514d563d62dd457abc
blockdoth <pepijn.pve@gmail.com>
2025-09-16 18:47:15 +0200

    [desktop@blockdoth] (18:47:11 25.11) System Generation 302

--- modules/system/services/network/pihole.nix
+++ modules/system/services/network/pihole.nix
@@ -6,8 +6,9 @@
   ...
 }:
 let
-  module = config.system-modules.services.network.blocky;
+  module = config.system-modules.services.network.pihole;
   domain = config.system-modules.secrets.domains.homelab;
+  port = 5125;
 in
 {
   config = lib.mkIf module.enable {
@@ -24,6 +25,25 @@ in
     services.pihole-web = {
       enable = true;
       hostName = "pihole.${domain}";
+      ports = [ "${builtins.toString port}" ];
     };
+
+    system-modules.services = {
+      network.reverse-proxy.proxies = [
+        {
+          subdomain = "pihole";
+          port = port;
+        }
+      ];
+      # TODO
+      # observability.healthchecks.endpoints = [
+      #   {
+      #     name = "Pihole";
+      #     url = "https://pihole.${domain}";
+      #     endpoint = "/health";
+      #   }
+      # ];
+    };
+
   };
 }
