b87f21196cd2c18e609f4d44753ecf01917cea2c
blockdoth <pepijn.pve@gmail.com>
2025-09-21 01:34:15 +0200

    [desktop@blockdoth] (23:43:00 25.11) System Generation 309

--- modules/system/display/default.nix
+++ modules/system/display/default.nix
@@ -1,29 +1,12 @@
 {
-  config,
-  lib,
-  pkgs,
   ...
 }:
-let
-  waylandEnabled = config.system-modules.display.wayland.enable;
-  x11Enabled = config.system-modules.display.x11.enable;
-in
 {
   imports = [
     ./wayland.nix
     ./x11.nix
     ./greeter.nix
+    ./portals.nix
   ];
 
-  config = lib.mkIf (waylandEnabled || x11Enabled) {
-    xdg.portal = {
-      enable = true;
-      wlr.enable = true;
-      config.common.default = "*";
-      extraPortals = with pkgs; [
-        xdg-desktop-portal
-        xdg-desktop-portal-gtk
-      ];
-    };
-  };
 }
---
+++ modules/system/display/portals.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  waylandEnabled = config.system-modules.display.wayland.enable;
+  x11Enabled = config.system-modules.display.x11.enable;
+in
+{
+  config = lib.mkIf (waylandEnabled || x11Enabled) {
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      config.common.default = "*";
+      extraPortals = with pkgs; [
+        xdg-desktop-portal
+        xdg-desktop-portal-gtk
+      ];
+    };
+  };
+}
