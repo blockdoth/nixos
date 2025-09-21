d710bbbbf2c12a21bb80863f722db6fa1a870914
blockdoth <pepijn.pve@gmail.com>
2025-09-21 12:14:40 +0200

    [desktop@blockdoth] (12:14:36 25.11) System Generation 316

--- modules/system/display/portals.nix
+++ modules/system/display/portals.nix
@@ -14,6 +14,7 @@ in
       enable = true;
       wlr.enable = true;
       extraPortals = with pkgs; [
+        xdg-desktop-portal
         xdg-desktop-portal-gtk
       ];
       config.common.default = [
