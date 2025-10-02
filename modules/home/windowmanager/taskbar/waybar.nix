aedb6ac2120c98c839e9ecf42813c51ee1b2a8a2
blockdoth <pepijn.pve@gmail.com>
2025-10-02 20:52:11 +0200

    [laptop@blockdoth] (2025-10-02 20:52) Home Generation 540

--- modules/home/windowmanager/taskbar/waybar.nix
+++ modules/home/windowmanager/taskbar/waybar.nix
@@ -173,11 +173,11 @@ in
             format = "{icon}";
             on-click = "activate";
             format-icons = {
-              "1" = "";
-              "2" = "";
-              "3" = "";
-              "4" = "";
-              "5" = "";
+              "1" = "1";
+              "2" = "2";
+              "3" = "3";
+              "4" = "4";
+              "5" = "5";
               urgent = "!";
               default = "*";
               sort-by-number = true;
