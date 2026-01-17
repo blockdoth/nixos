d670891ded34be07ce9269c29cf3def103ae3765
blockdoth <pepijn.pve@gmail.com>
2026-01-17 13:11:50 +0100

    [desktop@blockdoth] (2026-01-17 13:11) Home Generation 1100

--- modules/home/windowmanager/hyprland/layout.nix
+++ modules/home/windowmanager/hyprland/layout.nix
@@ -15,6 +15,20 @@ in
         "eDP-1,preferred,0x0,1"
       ];
 
+      workspace = [
+        "1, monitor:DP-2"
+        "2, monitor:DP-2"
+        "3, monitor:DP-2"
+        "4, monitor:DP-2"
+        "5, monitor:DP-2"
+
+        "6, monitor:DP-1"
+        "7, monitor:DP-1"
+        "8, monitor:DP-1"
+        "9, monitor:DP-1"
+        "10, monitor:DP-1"
+      ];
+
       xwayland = {
         force_zero_scaling = true;
       };
