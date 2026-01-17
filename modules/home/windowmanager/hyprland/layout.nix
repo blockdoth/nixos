bacb0f57c31188073c8921e91420f20da82de2cc
blockdoth <pepijn.pve@gmail.com>
2026-01-17 15:15:23 +0100

    [desktop@blockdoth] (2026-01-17 15:15) Home Generation 1110

--- modules/home/windowmanager/hyprland/layout.nix
+++ modules/home/windowmanager/hyprland/layout.nix
@@ -10,27 +10,27 @@ in
   config = lib.mkIf module.enable {
     wayland.windowManager.hyprland.settings = {
       monitor = [
-        "DP-2,2560x1440@143.972Hz,0x0,1"
-        "DP-1,1920x1200@99.94Hz,2560x-310,1,transform,3"
+        "DP-1,2560x1440@143.972Hz,0x0,1"
+        "DP-2,1920x1200@99.94Hz,2560x-310,1,transform,3"
         "eDP-1,preferred,0x0,1"
       ];
 
       workspace = [
-        "1, monitor:DP-2, default:true"
-        "2, monitor:DP-2"
-        "3, monitor:DP-2"
-        "4, monitor:DP-2"
-        "5, monitor:DP-2"
-        "6, monitor:DP-2"
-        "7, monitor:DP-2"
+        "1, monitor:DP-1, default:true"
+        "2, monitor:DP-1"
+        "3, monitor:DP-1"
+        "4, monitor:DP-1"
+        "5, monitor:DP-1"
+        "6, monitor:DP-1"
+        "7, monitor:DP-1"
 
-        "11, monitor:DP-1, default:true"
-        "12, monitor:DP-1"
-        "13, monitor:DP-1"
-        "14, monitor:DP-1"
-        "15, monitor:DP-1"
-        "16, monitor:DP-1"
-        "17, monitor:DP-1"
+        "11, monitor:DP-2, default:true"
+        "12, monitor:DP-2"
+        "13, monitor:DP-2"
+        "14, monitor:DP-2"
+        "15, monitor:DP-2"
+        "16, monitor:DP-2"
+        "17, monitor:DP-2"
       ];
 
       xwayland = {
