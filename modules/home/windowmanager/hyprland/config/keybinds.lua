818b8c9c0122d8a6804393f6eaf55ddb9da3f4d0
blockdoth <pepijn.pve@gmail.com>
2026-09-03 21:45:34 +0200

    [desktop@blockdoth] (2026-09-03 21:45) Home Generation 1165

--- modules/home/windowmanager/hyprland/config/keybinds.lua
+++ modules/home/windowmanager/hyprland/config/keybinds.lua
@@ -17,13 +17,15 @@ exec("G", "ghostty")
 
 
 bind("H", function()
-    if hl.get_workspace("special:minimized") then
-        hl.dispatch(hl.dsp.window.move({ workspace = hl.get_active_workspace(), window = "tag:minimized" }))
-        hl.dispatch(hl.dsp.window.clear_tags({ window = "tag:minimized" }))
-    else
-        hl.dispatch(hl.dsp.window.tag({ tag = "minimized", window = hl.get_active_window() }))
-        hl.dispatch(hl.dsp.window.move({ workspace = "special:minimized", follow = false }))
-    end
+    hl.dispatch(hl.dsp.workspace.toggle_special("magic"))
+    hl.dispatch(hl.dsp.window.move({ workspace = "+0" }))
+    hl.dispatch(hl.dsp.workspace.toggle_special("magic"))
+    hl.dispatch(hl.dsp.window.move({ workspace = "special:magic" }))
+    hl.dispatch(hl.dsp.workspace.toggle_special("magic"))
+end)
+
+hl.bind("SUPER + X", function ()
+
 end)
 
 exec("L", "pidof hyprlock | hyprlock")
