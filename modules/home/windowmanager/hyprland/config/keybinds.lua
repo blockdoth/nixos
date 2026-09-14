aed528db92f9c7abc3255342e5b7ea92d632f4de
blockdoth <pepijn.pve@gmail.com>
2026-09-15 00:00:25 +0200

    [desktop@blockdoth] (2026-09-15 00:00) Home Generation 1188

--- modules/home/windowmanager/hyprland/config/keybinds.lua
+++ modules/home/windowmanager/hyprland/config/keybinds.lua
@@ -51,7 +51,7 @@ exec("SHIFT + W", "wlogout-script")
 exec("Y", "zeditor ~/nixos")
 exec("Z", "pypr zoom")
 exec("SHIFT + Z", "pypr zoom ++0.6")
-exec("SHIFT + S", "grimblast copysave area")
+exec("SHIFT + S", "grimblast copysave area --notify ")
 
 ------------------------------------------------------------
 -- Focus / move
